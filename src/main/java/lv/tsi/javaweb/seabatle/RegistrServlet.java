7453a657f1b7d2a0efdbb728c7dbc1c6c91ef57b
evgshul <4ebura6ka>
2018-01-25 20:46:24 +0200

    less1 less2

--- src/main/java/lv/tsi/javaweb/seabatle/RegistrServlet.java
+++ src/main/java/lv/tsi/javaweb/seabatle/RegistrServlet.java
@@ -1,5 +1,7 @@
 package lv.tsi.javaweb.seabatle;
 
+import lv.tsi.javaweb.seabatle.model.Player;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,19 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "TestServlet", urlPatterns = "/runonserver")
-public class TestServlet extends HttpServlet {
+@WebServlet(name = "RegistrServlet", urlPatterns = "/register")// budet reagirovatj na slovo registr
+public class RegistrServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("player-name");
+        Player player = new Player();
+        player.setName(name);
 
+        response.sendRedirect("waitEnemyRegister");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Hoorey");
 
-
-        String name = request.getParameter("name");
-
-        response.getWriter().print("Hello, " + name);
-
+        request.getRequestDispatcher("/WEB-INF/register.jsp")
+        .include(request, response);
     }
 }
---
+++ src/main/java/lv/tsi/javaweb/seabatle/model/Player.java
@@ -0,0 +1,13 @@
+package lv.tsi.javaweb.seabatle.model;
+
+public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
